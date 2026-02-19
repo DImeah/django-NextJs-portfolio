3989173c1087164e9ec2e31b7edd6b2498b8af38
Imeah Dornubari <imeahdornu@gmail.com>
2026-02-19 21:35:46 +0100

    first commit

---
+++ backend/projects/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    tech_stack = models.JSONField(default=list)
+    github_url = models.URLField(blank=True)
+    live_url = models.URLField(blank=True)
+    image = models.ImageField(upload_to='projects/', blank=True)
+    featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
